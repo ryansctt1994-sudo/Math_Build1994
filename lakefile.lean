644631a434413248c2da0674847a31bd1926d646
ryansctt1994-sudo <ryan.sctt1994@gmail.com>
2026-06-03 18:29:32 -0500

    Add Math Build v3.1 Lean package

---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package «math-build-v3» where
+  -- Minimal Day-1 mathematical operating system package.
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib MathBuild where
+  -- Library root: MathBuild/
