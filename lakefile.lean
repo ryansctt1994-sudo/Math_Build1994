52641a2a51525c81f60dbf2b2d829d60af45fc63
ryansctt1994-sudo <ryan.sctt1994@gmail.com>
2026-06-03 21:06:27 -0500

    Pin mathlib dependency branch in lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package «math-build-v3» where
   -- Minimal Day-1 mathematical operating system package.
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "master"
 
 @[default_target]
 lean_lib MathBuild where
