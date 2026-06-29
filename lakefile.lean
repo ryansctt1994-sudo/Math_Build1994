41d2c9ae8a362a79de4f95733adf0fd8b53a151c
ryansctt1994-sudo <ryan.sctt1994@gmail.com>
2026-06-29 14:03:17 -0500

    fix deps pin mathlib to v4.31.0-rc1 commit

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package «math-build-v3» where
   -- Minimal Day-1 mathematical operating system package.
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "master"
+  "https://github.com/leanprover-community/mathlib4.git" @ "d568c8c09630de097a046763c17b9ea99f95f950"
 
 @[default_target]
 lean_lib MathBuild where
